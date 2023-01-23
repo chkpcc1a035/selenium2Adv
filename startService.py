78c1cd73caafcef5937683b5fdcf55b90a7abdd0
chkpcc1a035 <91876308+chkpcc1a035@users.noreply.github.com>
2023-01-24 01:39:49 +0800

    Uploaded chrome webdriver
    Uploaded mac_arm, win, linux chromium webdriver and init the startService.py

--- startService.py
+++ startService.py
@@ -1,2 +1,8 @@
 from selenium import webdriver
 
+''' under Windows Environment '''
+driver = webdriver.Chrome("./win_webdriver/chromedriver")
+''' under Mac_ARM Environment '''
+## driver = webdriver.Chrome("./mac_arm_webdriver/chromedriver")
+''' under Linux Environment '''
+## driver = webdriver.Chrome("./linux_webdriver/chromedriver")
